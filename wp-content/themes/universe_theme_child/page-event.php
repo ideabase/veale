43f2f03256a06b9663a04d1f2fac1a3575b0f1b4
Christopher Hallahan <challahan@gmail.com>
2017-08-11 12:19:35 -0400

    Update CSS and thinkbig event landing page

--- wp-content/themes/universe_theme_child/page-event.php
+++ wp-content/themes/universe_theme_child/page-event.php
@@ -17,119 +17,136 @@ get_header(); ?>
                        <div class="widget-inner">
 
             <header class="event-header">
-           	<img src="/wp-content/themes/universe_theme_child/ui/light-bulb-downsized.jpg" alt="Lightbulb" />
-              <img src="/wp-content/themes/universe_theme_child/ui/thinkBIGlogo-01.svg" alt="thinkBIG! - Veale Innovation Challenge & Summit"/>
-           		<h2>Join Us For the Challenge</h2>
-               <h3>2.27.18</h3>
-               <!-- Display the countdown timer in an element -->
-               <p id="demo"></p> <!-- Time element !-->
-
+             	<div class="event-header__image">
+             	<img class="lightbulb" src="/wp-content/themes/universe_theme_child/ui/light-bulb-downsized.jpg" alt="Lightbulb" />
+             	</div>
+             	<div class="event-header__text">
+                <img class="event-header__logo" src="/wp-content/themes/universe_theme_child/ui/thinkBIGlogo-01.svg" alt="thinkBIG! - Veale Innovation Challenge & Summit"/>
+             		<h2 class="headtext__title">Join Us For the Challenge</h2>
+                 <h3 class="headtext__date">Feb 27, 2018</h3>
+                 <h1 class="headtext__countdown"><p id="demo"></p></h1>
+             	</div>
              </header>
 
-      <section class="event-body">
-     <img src="/wp-content/themes/universe_theme_child/ui/placeholder-image.jpg" alt="Students" />
-     <div class="event-introduction">ThinkBIG! is a high school innovation Challenge and Summit that inspires youth to use creativity and collaboration to create big, innovative busi- ness ideas that promise to make Northeast Ohio even better.</div>
-     <h2>Join Us in 3 Easy Steps</h2>
-     <h3>Step 1: Register your school</h3>
-      <p>Don't worry, you don't have to know all the details</p>
-     <a href="/school-partner-registration/"> Register School</a>
-     <h3>Step 2: Submit Challenge Entries</h3>
-         <ul>
-           <li>$9,000 in Prizes</li>
-            <li> Be named a Veale Innovator </li>
-             <li> Prizes for Your School </li>
-            <li> Traveling Trophy</li>
-      	</ul>
-     <a href="/thinkbig-challenge-entry-submission/"> Submit Entries & View Resources</a>
-     <h3>Step 3: Attend the Summit</h3>
-     <p>Inspire your students. Plus, it's free! (Get more info & register.)</p>
-      <a href="/summit-registration/"> Find Out More</a>
-      </section>
+<section class="event-body">
+	<h3 class="callout">$9,000 in prizes!</h3>
+<div class="event-feature">
+  <?php
+  if ( has_post_thumbnail() ) {
+  	the_post_thumbnail('full');
+  } ?>
+	</div>
+<div class="event-introduction"><?php the_content(); ?></div>
+<div class="event-introduction__triangle"></div>
 
+<h2 class="event-h2">Join Us in 3 Easy Steps</h2>
 
+<div class="step-container step-container--1">
+	<div class="step-container__image">
+		<img class="circle circle1" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/259273/cir1_720.png">
+	</div>
+	<div class="step-container__details step-container__details--step-1">
+		<h3 class="step-1 step">Step 1:</h3>
+		<h3 class="step-title">Register your school</h3>
+	 <p class="step-description">Don't worry, you don't have to know all the details</p>
+	<a class="btn step-1-btn" href="/school-partner-registration/"> Register School</a>
+		</div>
+	</div>
 
+	<div class="step-container step-container--2">
+		<div class="step-container__details step-container__details--step-2">
+			<h3 class="step-2 step">Step 2:</h3>
+			<h3 class="step-title">Submit Challenge Entries</h3>
+			<ul class="step-description">
+				<li>$9,000 in Prizes</li>
+				 <li> Be named a Veale Innovator </li>
+					<li> Prizes for Your School </li>
+				 <li> Traveling Trophy</li>
+		</ul>
+	<a class="btn step-2-btn" href="/thinkbig-challenge-entry-submission/"> Submit Entries & View Resources</a>
+		</div>
+		<div class="step-container__image">
+			<img class="circle circle2" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/259273/cir2_720.png">
+		</div>
+	</div>
 
+	<div class="step-container step-container--3">
+		<div class="step-container__image">
+			<img class="circle circle3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/259273/cir3_720.png">
+		</div>
+		<div class="step-container__details step-container__details--step-3">
+			<h3 class="step-3 step">Step 3:</h3>
+			<h3 class="step-title">Attend the Summit</h3>
+	<p class="step-description">Inspire your students. Plus, it's free!</p>
+	 <a class="btn step-3-btn" href="#summit"> Find Out More</a>
+		</div>
+	</div>
+ </section>
 
-
-
-
-<!--
-              <section>
-                <h4>Presenting Sponsors</h4>
-                <h5>Sponsors:</h5>
-                <ul>
-                  <li>123 Partners</li>
-                </ul>
-
-                <h5>Partners:</h5>
-                <ul>
-                  <li>Geauga Growth Partnership</li>
-                  <li>Others</li>
-                </ul>
-
-                <h5>Participating Schools</h5>
-                <ul>
-                  <li>ABC High School</li>
-                  <li>Others</li>
-                </ul>
-
-                <h5>Advisors</h5>
-                <ul>
-                  <li>Connor Blakely</li>
-                  <li>Michael Hudeck</li>
-                  <li>Nicole McGee</li>
-                  <li>Others to be determined</li>
-                </ul>
-              </section>
-!-->
-
-<section class="event-details">
-<h2> The thinkBIG! Summit </h2>
+<section id="summit" class="event-details">
+<h2 class="event-details__title"> The thinkBIG! Summit </h2>
 <div class="event-details-introduction">
-<h3> What is the thinkBIG! Summit? </h3>
-<p> A unique, one-day conference featuring panels of
+	<div class="what-is">
+<h3 class="what-is__title"> What is the thinkBIG! Summit? </h3>
+<p class="what-is__description"> A unique, one-day conference featuring panels of
 national and local Gen Z and millennial entrepreneurs,
 mini maker hackathon sessions, Final Four Challenge team pitches, awards, prizes, networking and more!
 The Summit is open to all students, whether or not they compete in the Challenge. </p>
-<h4>Our Lineup, Speakers, & Agenda are Coming Soon!</h4>
+	</div>
+<h4 class="coming-soon">Our Lineup, Speakers, & Agenda are Coming Soon!</h4>
 </section>
-
 <section class="event-partners">
-	<h3>Sponsors & Partners</h3>
-	<h4>Sponsors</h4>
+	<h2 class="event-h2">Sponsors & Partners</h2>
+	<div class="sponsor-partner">
+	<h4 class="sponsor-title">Sponsors</h4>
 	<ul>
 		<li>Sponsor Logo</li>
 		<li>Sponsor Logo</li>
+		<li>Sponsor Logo</li>
+		<li>Sponsor Logo</li>
 	</ul>
-	<h4>Partners</h4>
+	</div>
+	<div class="sponsor-partner">
+	<h4 class="sponsor-title">Partners</h4>
 	<ul>
 		<li>Partner Logo</li>
 		<li>Partner Logo</li>
+		<li>Partner Logo</li>
+		<li>Partner Logo</li>
 	</ul>
-	<h4>Participating Schools</h4>
-	<ul>
-		<li>School Name</li>
-		<li>School Name</li>
-	</ul>
-	<h4>Advisors</h4>
-	<ul>
-		<li>Advisor Name</li>
-		<li>Advisor Name</li>
-	</ul>
+	</div>
+	<div class="school-advisor">
+		<div class="school-advisor__column">
+			<h4 class="sponsor-title">Participating Schools</h4>
+			<ul>
+				<li>School Name</li>
+				<li>School Name</li>
+			</ul>
+		</div>
+		<div class="school-advisor__column">
+		<h4 class="sponsor-title">Advisors</h4>
+			<ul>
+				<li>Advisor Name</li>
+				<li>Advisor Name</li>
+			</ul>
+			</div>
+	</div>
 </section>
+<footer class="event-footer">
+	<h3 class="event-footer__title">Contact thinkBIG!</h3>
+	<div class="event-footer__info">
+		<div class='event-footer__left'>
+		<a class="email" href="mailto:info@vealefound.org"> info@vealefound.org</a>
+		<p>216.255.3179</p>
+		</div>
+		<div class='event-footer__right'>
+		<address>30195 Chagrin Blvd. Suite 310<br /> Pepper Pike, Ohio 44124 </address>
+		</div>
+	</div>
+</footer>
 
 <script src="/wp-content/themes/universe_theme_child/event-scripts.js"></script>
 
-
-<footer class="event-footer">
-	<h3>Contact thinkBIG!</h3>
-	<a href="mailto:info@vealefound.org"> info@vealefound.org</a>
-  <p>216.255.3179</p>
-  <address>30195 Chagrin Blvd. Suite 310 Pepper Pike, Ohio 44124 </address>
-</footer>
-
-
-                <?php the_content(); ?>
 								<?php wp_link_pages(); ?>
                        </div> <!-- /.widget-inner -->
                    </div> <!-- /.widget-main -->

a07cd448ba025efeb273c749b713b1f832fbbb62
Nick Pappas <npappas2@kent.edu>
2017-08-16 17:43:31 -0400

    accordion commented in

--- wp-content/themes/universe_theme_child/page-event.php
+++ wp-content/themes/universe_theme_child/page-event.php
@@ -91,6 +91,196 @@ national and local Gen Z and millennial entrepreneurs,
 mini maker hackathon sessions, Final Four Challenge team pitches, awards, prizes, networking and more!
 The Summit is open to all students, whether or not they compete in the Challenge. </p>
 </div>
+
+<!--Phase Two Development
+<div class="togglebox">
+
+  <input id="toggle1" type="radio" name="toggle" />
+  <label for="toggle1">Speaker & Panelist Lineup</label>
+
+  <section class="content" id="content1">
+    <h2>Keynote Speaker</h2>
+    <ul>
+      <li><strong>GERARD ADAMS</strong></li>
+
+    <li>Millennial Mentor,</li>
+    <li>thought leader,</li>
+    <li>entrepreneur, investor,</li>
+    <li>philanthropist</li>
+      <li><a href="http://gerardadams.com/">http://gerardadams.com</a></li>
+       </ul>
+    <h2>Speakers</h2>
+    <ul>
+      <li><strong>Emcee</strong></li>
+      <li>Justin Lafazan</li>
+      <li>Next Gen Summit, Entrepreneur, Investor,</li>
+      <li>Speaker, Author</li>
+      <li><a href="www.justinlafazan.com">www.justinlafazan.com</a></li>
+    </ul>
+    <h2>Panelists</h2>
+    <ul class="panelist">
+      <li><strong>Panelist</strong></li>
+      <li>Brennan Agranoff</li>
+      <li>Founder & CEO,</li>
+      <li>Hoopswagg</li>
+      <li><a href="hoopswagg.com">hoopswagg.com</a></li>
+    </ul>
+    <ul class="panelist">
+      <li><strong>Panelist</strong></li>
+      <li>Deep Patel</li>
+      <li>Entrepreneur, marketer,</li>
+      <li>author</li>
+      <li><a href="deeppatel.com">deeppatel.com</a></li>
+    </ul>
+    <ul class="panelist">
+      <li><strong>Panelist</strong></li>
+      <li>Benjamin Stern</li>
+      <li>Founder, Nohbo</li>
+      <li><a href="nohboball.com">nohboball.com</a></li>
+    </ul>
+    <ul class="panelist">
+      <li><strong>Panelist</strong></li>
+      <li>Motivational</li>
+      <li>speaker, youth</li>
+      <li>health advocate,</li>
+      <li>vegan chef</li>
+      <li><a href="hailevthomas.com">hailevthomas.com</a></li>
+    </ul>
+
+    <h3>Local Hustle Team</h3>
+    <p>Will Lynch, Lynch Design • Matt Stauffer, 21 Sox • Giles Foster, Brews Brothers • Quiyanni Smith, Cakies</p>
+  </section>
+
+  <input id="toggle2" type="radio" name="toggle" />
+  <label for="toggle2">Agenda</label>
+
+  <section class="content" id="content2">
+    <table class="content_agenda">
+      <tr>
+        <td>8:30am</td>
+        <td>check in</td>
+      </tr>
+      <tr>
+        <td>9:00am</td>
+        <td>Welcome and opening remarks
+          <ul class="table_ul">
+            <li>Cynthia Bailie, Executive Director, Veale Youth Entrepreneurship Forum</li>
+            <li>Justin Lafazan, Emcee, thinkBIG! Summit, Co-founder, Next Gen Summit</li>
+          </ul>
+        </td>
+      </tr>
+      <tr>
+        <td>9:15am</td>
+        <td><em>Drive</em> - Gerard Adams<br />Gerard Adams became a self-made, multi-millionaire at the age of 24, having overcome many personal and financial obstacles to create the much-loved news platform EliteDaily.com, which sold for $50 million. A serial entrepreneur, angel investor, and philanthropist, Gerard currently dedicates his time to mentoring the next generation of creators and innovators to believe that the lives they dream of are not out of reach, but attainable through leadership, creativity, self confidence, and drive.
+</td>
+      </tr>
+      <tr>
+        <td>10:00am</td>
+        <td>Innovation Challenge FINAL FOUR Presentations</td>
+      </tr>
+      <tr>
+        <td>10:30am</td>
+        <td>BREAK</td>
+      </tr>
+      <tr>
+        <td>10:45am</td>
+        <td>Mini Maker Hackathon  - Presented by Upcycle Parts Shop</td>
+      </tr>
+      <tr>
+        <td>12:30pm</td>
+        <td>Lunch</td>
+      </tr>
+      <tr>
+        <td>1:15pm</td>
+        <td>
+          <em>Phenoms!</em> - Justin Lafazan, Moderator<br />
+          Invited teen innovators deliver Pecha Kucha (20x20) or IGNITE (20x15) style, 5-6 minute talks, Q&A
+<ol class="table_ol">
+  <li>Brennan Agranoff, Founder, HoopSwagg (confirmed, Oregon)</li>
+  <li>Deep Patel, Author, A Paperboy's Fable: The 11 Principles of Success (confirmed, Georgia)</li>
+  <li>Benjamin Stern  - Nohbo Shampoo pod shark tank Mark Cuban investee  (confirmed, Melbourne, FL)</li>
+  <li>Haile Thomas – healthy food advocate; founder, the HAPPY Org (confirmed, NYC)</li>
+          </ol></td>
+      </tr>
+      <tr>
+        <td>2:15pm</td>
+        <td>BREAK + Mini Maker Vote</td>
+      </tr>
+      <tr>
+        <td>2:30pm</td>
+        <td>
+          <em>Hustling in the Land</em> - Justin Lafazan, Moderator
+          <br />
+          Teen founders from NEO (current Veale students and Veale alum, if available)
+          <ol class="table_ol">
+            <li>Will Lynch (confirmed)(US, JCU)</li>
+            <li>Giles and Noah Foster (SHHS)</li>
+            <li>Quiyanni Smith (Beaumont, Miami)</li>
+            <li>Matthew Stauffer (Benedictine)</li>
+          </ol>
+        </td></tr>
+      <tr>
+        <td>3:30pm</td>
+        <td>Mini Maker Hackathon - Final Four Demonstrations and Audience Noisemaker Vote</td>
+      </tr>
+      <tr>
+        <td>4:00pm</td>
+        <td>Innovation Challenge Awards</td>
+      </tr>
+      <tr>
+        <td>4:30pm</td>
+        <td>Gerard Adams - Inspirational Wrap Up</td>
+      </tr>
+      <tr>
+        <td>5:00pm</td>
+        <td>Closing, Reception</td>
+      </tr>
+    </table>
+  </section>
+
+  <input id="toggle3" type="radio" name="toggle" />
+  <label for="toggle3">Mini Maker Hackathon</label>
+
+  <section class="content" id="content3">
+    <p>
+      A sprint-like design event that includes team members of various subject matter expertise who collaborate intensively to generate ideas and/or a product. The term originated with computer programmers and software developers.
+    </p>
+    <a class="hack-btn" href="">Learn More About the Hackathon</a>
+  </section>
+
+  <input id="toggle4" type="radio" name="toggle" />
+  <label for="toggle4">Sponsors & Partners</label>
+
+  <section class="content" id="content4">
+    <h2>Sponsors & Partners</h2>
+	<h4>Sponsors</h4>
+	<ul>
+		<li><a target="_blank" href="http://connorblakley.com"><img src="/wp-content/themes/universe_theme_child/ui/connor-blakeley.png" alt="YouthLogic" /></a></li>
+		<li><a target="_blank" href="http://www.case.edu/universitycenter"><img src="/wp-content/themes/universe_theme_child/ui/tinkham-veale.jpeg" alt="TVUC@CWRU" /></a></li>
+    <li><a target="_blank" href="http://fownders.com/about/"><img src="/wp-content/themes/universe_theme_child/ui/fownders.png" alt="Fownders" /></a></li>
+	</ul>
+	<h4>Partners</h4>
+	<ul>
+		<li><a target="_blank" href="http://www.gcesc.k12.oh.us/"><img src="/wp-content/themes/universe_theme_child/ui/geauga-logo.png" alt="Geauga ESC" /></a></li>
+			<li><a target="_blank" href="https://www.ngsummit.com"><img src="/wp-content/themes/universe_theme_child/ui/Nextgen.png" alt="NextGen Summit" /></a></li>
+      <li><a  target="_blank" href="https://www.upcyclepartsshop.org"><img src="/wp-content/themes/universe_theme_child/ui/uncycle-parts.jpg" alt="Upcycle Parts Shop" /></a></li>
+	</ul>
+	<h4>Participating Schools</h4>
+	<ul>
+		<li>School Name</li>
+		<li>School Name</li>
+	</ul>
+	<h4>Advisors</h4>
+	<ul>
+		<li>Advisor Name</li>
+		<li>Advisor Name</li>
+	</ul>
+  </section>
+
+</div>
+
+!-->
+
 <div class="event-details-introduction">
 
   <h2 class="event-h2">Speakers</h2>
@@ -155,6 +345,7 @@ The Summit is open to all students, whether or not they compete in the Challenge
 		</div>
 	</div>
 </footer>
+
 
 <script src="/wp-content/themes/universe_theme_child/event-scripts.js"></script>
 
